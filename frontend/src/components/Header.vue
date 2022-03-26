0d293d59e31ac24eed696d4057df98c828314575
jarilton <jariltonpereira02@hotmail.com>
2022-03-26 18:17:50 -0300

    feat: menu do cabeçalho responsivo

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,7 +5,21 @@
 
     <a href="/"><img alt="Carrinho de compras" id="logo" src="../assets/logo.jpg" /> </a>
 
-    <img alt="Abrir menu" id="menu-button" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg" />
+    <img v-on:click="openMenu" alt="Abrir menu" id="menu-button" src="../assets/button-logo.jpg" />
+
+    <div v-on:click="closeMenu" id="menu-overlay" v-if="menuActive"/> 
+
+    <div id="menu-items" :class="{active:menuActive}" >
+
+      <img alt="Carrinho de compras" id="menu-logo" src="../assets/logo.jpg" />
+
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/produtos">Produtos</a></li>
+        <li><a href="/sobre">Sobre</a></li>
+        <li><a href="/contato">Contato</a></li>
+      </ul>
+    </div>
 
     </nav>
 
@@ -14,7 +28,20 @@
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data() {
+    return {
+      menuActive: false,
+    }
+  },
+  methods: {
+    openMenu: function() {
+      this.menuActive = true;
+    },
+    closeMenu: function() {
+      this.menuActive = false;
+    }
+  }
 }
 </script>
 
@@ -31,6 +58,8 @@ export default {
   nav{
     display: flex;
     justify-content: space-between;
+    height: 60px;
+    align-items: center;
   }
 
   #logo {
@@ -38,6 +67,80 @@ export default {
   }
 
   #menu-button {
-    width: 30px;
+    width: 60px;  
+    height: 60px;
   }
+
+  #menu-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 40%;
+    height: 100vh;
+    background-color: #000000;
+    opacity: 0.8;
+  }
+
+  #menu-logo {
+    width: 40%;
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+
+  #menu-items{
+    position: fixed;
+    top: 0;
+    right: 0;
+    background-color: var(--color-background-nav);
+    width: 60%;
+    height: 100vh;
+    display: none;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #menu-items.active {
+    display: flex;
+  }
+
+  ul {
+    list-style: none;
+    text-align: center;
+  }
+
+  ul li {
+    margin: 20px 0;
+  }
+
+  ul li a {
+    color: var(--color-text-light);
+  }
+
+  @media (min-width: 700px) {
+    #menu-button,
+    #menu-logo,
+    #menu-overlay {
+      display: none;
+    }
+
+    #menu-items {
+      display: flex;
+      position: static;
+      height: 60px;
+      width: auto;
+    }
+
+    ul {
+      display: flex;
+      height: 60px;
+      align-items: center;
+    }
+
+    ul li {
+      margin: 0;
+      margin-left: 20px;
+    }
+  } 
+
 </style>
