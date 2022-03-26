52d0d5fcdc84a735a2c7e0620871e681c6edc47e
jarilton <jariltonpereira02@hotmail.com>
2022-03-26 20:07:14 -0300

    feat: rodapé

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+
+    <text class="text" id="texto">Desenvolvido por Jamal Frontend Developer</text>
+
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer',  
+  }
+</script>
+
+<style scoped>
+
+  footer {
+    background-color: var(--color-background-nav);
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #texto{
+    color: var(--color-text-light); 
+  }
+
+</style>
