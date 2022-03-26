2960c525e2e59334e3a52d02d867b6a5939df837
jarilton <jariltonpereira02@hotmail.com>
2022-03-26 19:12:42 -0300

    feat: tela home

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <Header />
+    <Home />
   </div>
 </template>
 
 <script>
+
 import './styles/global.css'
-import Header from'./components/Header'
+import Header from'./components/Header.vue'
+import Home from'./pages/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Home
   }
 }
 </script>
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <main>
+
+    <div class="text-top"> Sejam bem vindos a melhor operadora de <b>internet, tv e telefone</b> do brasil</div>
+
+    <div class="text-top"> <b>Clique abaixo para acessar os planos</b></div>
+
+    <a href="/produtos" target="_blank">
+      <img src="../assets/button-acesso.jpg" alt="ver planos" id="planos-internet">
+    </a>
+
+    <div class="text-bottom">jamalnet.com.br</div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home'  
+}
+</script>
+
+<style scoped>
+
+  main {
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text-top {
+    margin: 10px;
+    font-size: 22px;
+    margin-bottom: 20px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  #planos-internet {
+    width: 340px;
+  }
+
+  .text-bottom {
+    margin: 30px;
+    font-size: 18px;
+    margin-top: 20px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  @media (min-width: 700px) {
+    #planos-internet {
+      width: 370px;
+    }
+  }
+
+</style>
