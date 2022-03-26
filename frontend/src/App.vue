a0bb834472c5cfaa17ab4f5f6bda23ff6697b9d1
jarilton <jariltonpereira02@hotmail.com>
2022-03-26 16:34:11 -0300

    feat: cabeçalho da aplicação

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hello vue docker"/>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './styles/global.css'
+import Header from'./components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+
+    <nav class="container">
+
+    <a href="/"><img alt="Carrinho de compras" id="logo" src="../assets/logo.jpg" /> </a>
+
+    <img alt="Abrir menu" id="menu-button" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg" />
+
+    </nav>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  header {
+    background-color: var(--color-background-nav);
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  nav{
+    display: flex;
+    justify-content: space-between;
+  }
+
+  #logo {
+    width: 60px;
+  }
+
+  #menu-button {
+    width: 30px;
+  }
+</style>
