18e0223f1caba2344d4dad0d7c2df686764682c7
jarilton <jariltonpereira02@hotmail.com>
2022-03-27 17:46:12 -0300

    feat: tela de contato

--- frontend/src/views/Contato.vue
+++ frontend/src/views/Contato.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Contato</h1>
+  <main class="container">
+    <h1>Contato</h1>
+
+    <div class="text-top">
+      <h2>Email: jariltonpereira02@hotmail.com</h2>
+      <h2>GitHub: https://github.com/jarilton</h2>
+      <h2>Telefone whatsApp: (16) 99348-7729</h2>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -9,5 +17,23 @@ export default {
 </script>
 
 <style scoped>
+
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+
+.text-top {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+  margin-top: 70px;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
 
 </style>
